c91678154c4652efae73a32f2d32d409b338593b
aelintra <admin@aelintra.com>
2020-06-27 15:29:04 +0100

    initial build

--- sail-7.0.0/opt/sark/generator/srk7dbcvt.php
+++ sail-7.0.0/opt/sark/generator/srk7dbcvt.php
@@ -197,4 +197,26 @@ $custTables = array(
 
    	}
 
+/*
+ * Parse the new extensions and fixup the extended dials if necessary
+ */
+
+	$res = NULL;
+
+	$table = $v7dbh->query("select pkey,cluster from IPphone")->fetchall(PDO::FETCH_ASSOC);
+
+   	foreach ($table as $row ) {
+   		if (strlen($row['pkey'] <= 4)) {
+   			$id = $v7dbh->query("SELECT id FROM cluster WHERE pkey = '" . $row['cluster'] . "'")->fetch(PDO::FETCH_COLUMN);
+   			$sql = $v7dbh->prepare("UPDATE IPphone SET pkey = ? WHERE pkey = ?");
+   			$sql->execute(array($id . $row['pkey'], $row['pkey']));
+   			$res = NULL;
+   		}
+
+   	}
+
+
+
+
+
 	       
