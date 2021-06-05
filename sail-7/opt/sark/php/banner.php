368086bea84483afb7634c0df81917b440cc130d
aelintra <admin@aelintra.com>
2021-06-05 20:49:59 +0100

    change logo to 7

--- sail-7/opt/sark/php/banner.php
+++ sail-7/opt/sark/php/banner.php
@@ -5,7 +5,7 @@ if (file_exists("/opt/sark/www/sark-common/Customer_Branding_Colour_web.png")) {
 	echo '<img src="/sark-common/Customer_Branding_Colour_web.png" class="w3-padding w3-image" alt="SARK UCS" style="max-height: 4em;">' . PHP_EOL;
 }	
 else {
-	echo '<img src="/sark-common/Sark_Colour_web_2018.png" class="w3-padding w3-image" alt="SARK UCS" style="max-height: 4em;">' . PHP_EOL;	 
+	echo '<img src="/sark-common/Sark7_Colour_web.png" class="w3-padding w3-image" alt="SARK UCS" style="max-height: 4em;">' . PHP_EOL;	 
 }
 ?>
 </div>
